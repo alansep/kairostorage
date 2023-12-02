f1053ab1ff814a9aeb71f74cfbc7c0e10f05df95
alansep <alang.von@gmail.com>
2023-12-02 17:48:13 -0300

    Starting project

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Kairostorage = void 0;
+var Kairostorage = /** @class */ (function () {
+    function Kairostorage() {
+    }
+    Kairostorage.prototype.helloNpm = function () {
+        return "hello NPM";
+    };
+    return Kairostorage;
+}());
+exports.Kairostorage = Kairostorage;
---
+++ index.ts
@@ -0,0 +1,7 @@
+export class Kairostorage {
+
+    helloNpm(): string {
+        return "hello NPM";
+    }
+
+}
